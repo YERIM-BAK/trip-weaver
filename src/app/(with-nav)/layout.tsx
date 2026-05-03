1f41ab82ee293f91dee6ef820a5eb6dc4bf1a4ce
BAKYERIM <rumiobu03@gmail.com>
2026-05-03 13:28:44 +0900

    지도 script layout에 추가

--- src/app/(with-nav)/layout.tsx
+++ src/app/(with-nav)/layout.tsx
@@ -1,9 +1,14 @@
+import Script from "next/script";
 import Header from "@/components/layout/Header/Header";
 import TabBar from "@/components/layout/TabBar/TabBar";
 
 function WithNavLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Script
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&libraries=services,clusterer&autoload=false`}
+        strategy="afterInteractive"
+      />
       <Header />
       <main id="main-content" tabIndex={-1}>
         {children}
