8999f1462a729e46b99c13b899344c269a81e448
BAKYERIM <rumiobu03@gmail.com>
2026-05-03 13:29:32 +0900

    코스 타입 추가

--- src/features/course/course.types.ts
+++ src/features/course/course.types.ts
@@ -7,3 +7,19 @@ export type Course = {
   petAllowed: boolean;
   image: string;
 };
+
+export type Spot = {
+  name: string;
+  category: string;
+  description: string;
+  address: string;
+  duration: string;
+  lat: number;
+  lng: number;
+};
+
+export type DayCourse = {
+  day: number;
+  date: string;
+  spots: Spot[];
+};
