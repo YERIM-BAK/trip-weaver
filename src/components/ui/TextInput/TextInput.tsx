560d6495ee3b56793f115335d0b09e2671c02641
BAKYERIM <rumiobu03@gmail.com>
2026-05-05 15:48:15 +0900

    input 기본 동작 추가

--- src/components/ui/TextInput/TextInput.tsx
+++ src/components/ui/TextInput/TextInput.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import { forwardRef } from "react";
+import { forwardRef, useState } from "react";
 import styles from "./TextInput.module.scss";
 import { InputProps } from "./TextInput.types";
 import clsx from "clsx";
 
 const TextInput = forwardRef<HTMLInputElement, InputProps>(
   ({ label, id, type = "text", helperText, className, ...props }, ref) => {
+    const [text, setText] = useState("");
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setText(e.target.value);
+    };
+    const onReset = () => {
+      setText("");
+    };
     return (
       <div className={clsx(styles["inputField"], "inputField", className)}>
         <div className={clsx(styles["inputControl"], "inputControl")}>
@@ -19,15 +26,20 @@ const TextInput = forwardRef<HTMLInputElement, InputProps>(
             id={id}
             ref={ref}
             type={type}
+            onChange={handleInputChange}
+            value={text}
             className={clsx(styles["input"], "input")}
             {...props}
           />
 
-          <button
-            type="button"
-            className={clsx(styles["inputClearBtn"], "inputClearBtn")}
-            aria-label="내용 삭제"
-          ></button>
+          {text && (
+            <button
+              type="button"
+              className={clsx(styles["inputClearBtn"], "inputClearBtn")}
+              aria-label="내용 삭제"
+              onClick={onReset}
+            ></button>
+          )}
         </div>
         {helperText && <p className={styles["helperText"]}>메세지</p>}
       </div>
