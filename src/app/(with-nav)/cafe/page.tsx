1cc1f2023b05676a9c9bc2a985d9de55cbedce3c
BAKYERIM <rumiobu03@gmail.com>
2026-05-29 16:44:38 +0900

    tsx추가

---
+++ src/app/(with-nav)/cafe/page.tsx
@@ -0,0 +1,5 @@
+import CafePage from "@/pages/ExplorePage";
+
+export default function page({}) {
+  return <CafePage />;
+}
---
+++ src/pages/CafePage.tsx
@@ -0,0 +1,5 @@
+import styles from "./CafePage.module.scss";
+
+export default function CafePage({}) {
+  return <div className={styles.container}>카페</div>;
+}
