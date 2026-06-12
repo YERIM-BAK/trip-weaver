6acd72f4c8cb78776f342222d541c22ba98d8aee
BAKYERIM <rumiobu03@gmail.com>
2026-06-13 04:07:21 +0900

    스팟 상세 페이지 스켈레톤 제거

--- src/app/(with-nav)/spots/[id]/page.tsx
+++ src/app/(with-nav)/spots/[id]/page.tsx
@@ -4,29 +4,6 @@ import {
   getSpotIntroDetailServer,
 } from "@/lib/petTour/petTourApi.server";
 import SpotDetailPage from "@/views/spots/SpotDetailPage";
-import { Suspense } from "react";
-import Skeleton from "@/components/ui/Skeleton/Skeleton";
-
-async function SpotData({ id, typeId }: { id: string; typeId?: string }) {
-  const [common, pet, intro] = await Promise.all([
-    getSpotCommonDetailServer(id),
-    getPetTourInfoServer(id),
-    typeId ? getSpotIntroDetailServer(id, typeId) : Promise.resolve(null),
-  ]);
-
-  return <SpotDetailPage common={common} pet={pet} intro={intro} />;
-}
-
-function SpotSkeleton() {
-  return (
-    <div className="skeletonWrap">
-      <Skeleton height={280} />
-      <Skeleton height={28} />
-      <Skeleton height={20} />
-      <Skeleton height={20} />
-    </div>
-  );
-}
 
 export default async function Page({
   params,
@@ -38,9 +15,11 @@ export default async function Page({
   const { id } = await params;
   const { typeId } = await searchParams;
 
-  return (
-    <Suspense fallback={<SpotSkeleton />}>
-      <SpotData id={id} typeId={typeId} />
-    </Suspense>
-  );
+  const [common, pet, intro] = await Promise.all([
+    getSpotCommonDetailServer(id),
+    getPetTourInfoServer(id),
+    typeId ? getSpotIntroDetailServer(id, typeId) : Promise.resolve(null),
+  ]);
+
+  return <SpotDetailPage common={common} pet={pet} intro={intro} />;
 }
