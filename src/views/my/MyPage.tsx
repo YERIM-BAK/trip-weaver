a7aa5fdecd3dfe86294814db791ae910d1690d37
BAKYERIM <rumiobu03@gmail.com>
2026-06-12 19:45:34 +0900

    my 메인 반려동물 카드 작업

--- src/views/my/MyPage.tsx
+++ src/views/my/MyPage.tsx
@@ -4,8 +4,7 @@ import { Camera } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 import Link from "next/link";
-import ImgPetWalking from "@/assets/images/img/img-pet-walking.png";
-import PetCard from "@/features/pet/components/PetCard";
+import ImgPetDefault from "@/assets/images/img/icon-dog.png";
 import defaultAvatar from "@/assets/images/img/img-profile.png";
 
 const DEFAULT_AVATAR = defaultAvatar;
@@ -28,6 +27,8 @@ function MyPage() {
               <Image
                 src={profileImage ?? DEFAULT_AVATAR}
                 alt="프로필"
+                width={96}
+                height={96}
                 priority
               />
             </div>
@@ -76,7 +77,7 @@ function MyPage() {
         </ul>
       </div>
 
-      <div className="petSection">
+      <div className="section">
         <div className="titleWrap">
           <h2 className="sectionTitle">함께하는 반려동물</h2>
           <Link href="/my/pets" className="link-btn">
@@ -84,33 +85,36 @@ function MyPage() {
           </Link>
         </div>
 
-        <div className="petCard">
+        <Link href={`/my/pets`} className="petCard">
           <div className="petInfo">
             <div className="petImage">
-              <Image src="/images/pet.jpg" alt="몽이" fill sizes="100" />
+              <Image src={ImgPetDefault} alt="몽이" priority />
             </div>
 
             <div className="petContent">
-              <div className="petNameRow">
-                <strong className="petName">몽이</strong>
+              {/* {pet.isPrimary && (
+                <span className="petBadge">대표</span>
+              )} */}
+              <span className="petBadge">대표</span>
+              <strong className="petName">몽이</strong>
 
-                <button type="button" className="petEditBtn">
-                  수정
-                </button>
-              </div>
+              <button
+                type="button"
+                className="petEditBtn"
+                aria-label="반려동물 프로필 수정"
+              ></button>
 
-              <p className="petMeta">말티즈 · 5살 · 남아</p>
+              <p className="petMeta">말티즈&middot;5살&middot;남아</p>
             </div>
           </div>
-        </div>
+        </Link>
 
         <button type="button" className="addPetBtn">
-          <span className="addPetIcon">+</span>
           <span>반려동물 추가하기</span>
         </button>
       </div>
 
-      <div>
+      <div className="section">
         <div className="titleWrap">
           <h2 className="sectionTitle">내 여행</h2>
           <Link href="/my/pets" className="link-btn">
